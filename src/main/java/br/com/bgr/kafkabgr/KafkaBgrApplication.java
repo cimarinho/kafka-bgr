9681dd7d3882bd8cb40411bd6c6bf37f899036fa
Marcelo Marinho <marinho.marcelo@gmail.com>
2020-02-18 20:34:15 -0300

    bgr

---
+++ src/main/java/br/com/bgr/kafkabgr/KafkaBgrApplication.java
@@ -0,0 +1,13 @@
+package br.com.bgr.kafkabgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaBgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bgr/kafkabgr/KafkaBgrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bgr.kafkabgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
